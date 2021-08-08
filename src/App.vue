1cc4d6f7a93cdac7904d372caaa89989c9502669
Amanda Hasenzahl <amandahasenzahl@Amandas-MacBook-Pro.local>
2021-08-07 21:34:34 -0500

    Get basic flight information to output in template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="dashboard">
+        <div class="card"></div>
+        <div class="card -center"></div>
+        <div class="card">
+            <FlightCard
+                v-for="flight in this.flightDataList" 
+                :key="flight.id"
+                :flight="flight"
+            />
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FlightCard from './components/FlightCard.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        FlightCard
+    },
+    data() {
+        return {
+            flightDataList: []
+        };
+    },
+    created() {
+        this.getFlightData();
+    },
+    methods: {
+        getFlightData() {
+            fetch('flights.json')
+                .then(response => response.json())
+                .then(data => (this.flightDataList = data));
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/FlightCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flight-card__container">
+        <div class="flight-card__id">{{ flight.id }}</div>
+        <div class="flight-card__details">
+            <div class="flight-card__depart">
+                <p class="flight-card__origin">{{ flight.origin }}</p>
+                <p class="flight-card__depart-time">{{ flight.readable_departure }}</p>
+            </div>
+            <div class="flight-card__arrive">
+                <p class="flight-card__destination">{{ flight.destination }}</p>
+                <p class="flight-card__arrival-time">{{ flight.readable_arrival }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FlightCard',
+    props: ['flight']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
